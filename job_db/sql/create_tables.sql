170383c618504539c39c976031b9710d365dca22
ChickenBenny <zxc123benny14159@gmail.com>
2022-09-07 16:28:08 +0800

    <feat>: add init endpoint in backendserver
    -main: add init endpoint which will crawl and process the data from cakeresume
    
    -requirements: pandas and bs4 packages

--- job_db/sql/create_tables.sql
+++ job_db/sql/create_tables.sql
@@ -2,46 +2,56 @@
 -- we wil have 5 table such as backend engineer, data engineer, data scientist, machine learning engineer and QA engineer
 -- Each table will have 8 columns
 CREATE TABLE IF NOT EXISTS backend_engineer (
-    url_link TEXT NOT NULL,
+    url_link TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
     company TEXT NOT NULL,
     skill TEXT[],
     job_type TEXT,
     loc TEXT,
-    salary TEXT
+    salary TEXT,
+    dt DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS data_engineer (
-    url_link TEXT NOT NULL,
+    url_link TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
     company TEXT NOT NULL,
     skill TEXT [],
     job_type TEXT,
     loc TEXT,
-    salary TEXT
+    salary TEXT,
+    dt DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS data_scientist (
-    url_link TEXT NOT NULL,
+    url_link TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
     company TEXT NOT NULL,
     skill TEXT[],
     job_type TEXT,
     loc TEXT,
-    salary TEXT
+    salary TEXT,
+    dt DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ml_engineer (
-    url_link TEXT NOT NULL,
+    url_link TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
     company TEXT NOT NULL,
     skill TEXT[],
     job_type TEXT,
     loc TEXT,
-    salary TEXT
+    salary TEXT,
+    dt DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS qa_engineer (
-    url_link TEXT NOT NULL,
+    url_link TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
     company TEXT NOT NULL,
     skill TEXT[],
     job_type TEXT,
     loc TEXT,
-    salary TEXT
+    salary TEXT,
+    dt DATE NOT NULL
 );

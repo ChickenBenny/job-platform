91f9d4a0e2a240a83445bc0ded8f4a284489953b
ChickenBenny <zxc123benny14159@gmail.com>
2022-09-09 16:21:38 +0800

    <feat>: complete the init database part
    -main: init endpoint will crawl the job from cakeresume and yourator
    
    -crawl_information: add the crawling info into json file makes program more simple
    
    -init_database: will call crawling models from init folder and will get the data from cakeresume and yourator

---
+++ backendserver/models/init_database.py
@@ -0,0 +1,39 @@
+from models.init_models.crawler_cakeresume import web_crawler_cake
+from models.init_models.process_and_insert_cakeresume import preprocessing_cakeresume
+from models.init_models.crawler_yourator import crawler_yourator
+import json
+
+def get_information(website):
+    with open('/models/crawl_information.json', 'r') as fp:
+        information = json.load(fp)[website]
+        return information
+
+class init_crawler():
+    def __init__(self):
+        self.cakeresume = get_information('cakeresume')
+        self.yourator = get_information('yourator')
+
+    def init_cakeresumse(self):
+        data_processor = preprocessing_cakeresume()
+        for key in self.cakeresume.keys():
+            job_type = self.cakeresume[key]
+            type = 'init'
+            
+            web_crawler = web_crawler_cake(job_type, type)
+            url_data = web_crawler.get_url_data()
+            data = web_crawler.crawl_all_data(url_data)
+
+            data = data_processor.process_with_pandas(data)
+            data_processor.insert_into_db(data, key)
+
+    def init_yourator(self):
+        crawler = crawler_yourator()
+        for key in self.yourator.keys():
+            url_data = crawler.get_url(self.yourator[key])
+            data = crawler.get_data(url_data)
+            data = crawler.process_with_pandas(data)
+            crawler.insert(data, key)
+
+    def init_database(self):
+        self.init_cakeresumse()
+        self.init_yourator()

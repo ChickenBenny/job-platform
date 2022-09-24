ddbc52faef69ce3c12ee5e494a18ae9109281143
ChickenBenny <zxc123benny14159@gmail.com>
2022-09-24 10:32:22 +0800

    <build>: rebuild the frontend UI

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
   plugins: [react()]
 })

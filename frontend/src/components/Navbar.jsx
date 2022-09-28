3584a751baf1535f901c037524a45200d9886778
ChickenBenny <zxc123benny14159@gmail.com>
2022-09-28 14:29:53 +0800

    <feat>:logo link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { NavLink } from 'react-router-dom'
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="jobHunter" className="w-[124px] h-[50px]" />
+      <NavLink to="/"><img src={logo} alt="jobHunter" className="w-[124px] h-[50px]" /></NavLink>
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
